d39de697a444b9c92d84257dd6fa20480cf853ed
Vitor Marinho Faria <vitormarinhofaria@gmail.com>
2022-10-17 13:27:36 -0300

    Remove leftovers  and delete copy ctors/assignment

--- GravitySim/Quad2D.h
+++ GravitySim/Quad2D.h
@@ -3,8 +3,11 @@
 #include "Renderer.h"
 #include "ConstBuffer.h"
 
-#define INSTANCE_COUNT 800
-
+#ifdef NDEBUG
+#define INSTANCE_COUNT_DEF 1200
+#else
+#define INSTANCE_COUNT_DEF 200
+#endif
 class Quad2D
 {
 	ID3D11Buffer* vertBuffer = nullptr;
@@ -14,16 +17,23 @@ class Quad2D
 	ID3D11Buffer* instanceIndexBuffer = nullptr;
 	ID3D11Buffer* instanceDataBuffer = nullptr;
 
-	ID3D11Buffer* matricesBuffer = nullptr;
-	ID3D11Buffer* colorsBuffer = nullptr;
+	uint32_t mVerticesCount = 0;
+	uint32_t mInstancesCount = 0;
 
 public:
 	void Draw(Renderer& renderer);
 	void SetInput(Renderer& r);
-	Quad2D(Renderer& renderer);
+	Quad2D(Renderer& renderer, uint32_t count);
 	~Quad2D();	
-	std::vector<DirectX::SimpleMath::Matrix> mMatrices;
-	std::vector<DirectX::SimpleMath::Vector3> mColors;
+	//std::vector<DirectX::SimpleMath::Matrix> mMatrices;
+	//std::vector<DirectX::SimpleMath::Vector3> mColors;
 	std::vector<InstanceData> instanceData;
+
+	Quad2D& operator=(const Quad2D& other) = delete;
+	Quad2D(const Quad2D& other) = delete;
+	
+	Quad2D(Quad2D&& other) noexcept = delete;
+	Quad2D& operator=(Quad2D&& other) noexcept;
+
 };
 
